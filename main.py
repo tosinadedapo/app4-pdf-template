fbfc8fe6dca81250a703936e02260ebbd609b188
tosinadedapo <tosinoreka@yahoo.com>
2024-03-26 17:12:45 +0100

    pdf creation with footer

--- main.py
+++ main.py
@@ -2,15 +2,32 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
-    for i in range(row["Pages"]):
+
+    pdf.add_page()
+
+    pdf.set_font("Times", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    pdf.line(10, 21, 200, 21)
+
+    # Set the footer
+    pdf.ln(265)  # Added break lines
+    pdf.set_font("Times", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+    for i in range(row["Pages"] - 1):
         pdf.add_page()
-        pdf.set_font("Arial", size=14)
-        if i == 0:
-            pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-            pdf.line(10, 22, 200, 22)
+        # Set the footer
+        pdf.ln(277)  # Added break lines
+        pdf.set_font("Times", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
 
 pdf.output("output.pdf")
