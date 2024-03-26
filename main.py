1d7336016460c71c3aa768ef76657a2176f601c6
tosinadedapo <tosinoreka@yahoo.com>
2024-03-26 17:28:27 +0100

    pdf add Rule lines

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ for index, row in df.iterrows():
     pdf.set_font("Times", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 21, 200, 21)
+
+    # Rule a lines
+    for y in range(20,298,10):
+       pdf.line(10, y, 200, y)
+
 
     # Set the footer
     pdf.ln(265)  # Added break lines
@@ -29,5 +33,8 @@ for index, row in df.iterrows():
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
+        # Rule a lines
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output("output.pdf")
